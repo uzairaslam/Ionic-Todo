812dc04922f2da321e866e0ffff525904d85ea66
uzairaslam - Uzair Aslam <uzairaslam64@gmail.com>
2018-10-12 17:27:02 +0500

    Added add/delete todo functionality
    Added add todo and delete todo

--- todo/src/pages/home/home.ts
+++ todo/src/pages/home/home.ts
@@ -1,14 +1,48 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
+ public toDos = [];
+  constructor(public navCtrl: NavController, private alertCntrl: AlertController) {
 
   }
 
+  addTodoAlert() {
+    let addTodoAlert = this.alertCntrl.create({
+      title: "Add Todo",
+      message: "Add your Todo",
+      inputs: [
+        {
+          name: "todoName",
+          placeholder: "Todo Name"
+        }
+      ],
+      buttons: [
+        {
+          text: "Cancel",
+          role: "cancel"
+        },
+        {
+          text: "Add Role",
+          handler: data => {
+            let todoName;
+            todoName = data.todoName;
+            this.toDos.push(todoName);
+          }
+        }
+      ]
+    });
+    addTodoAlert.present();
+  }
+
+  removeTodo(todo:string) {
+    let index:number = this.toDos.indexOf(todo);
+    if(index !== -1) {
+      this.toDos.splice(index, 1);
+    }
+  }
 }
